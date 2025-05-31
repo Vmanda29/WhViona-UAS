a3fad69ab60a6de480ba8c4bc78b31da6ca880de
Vmanda29 <vmanda037@gmail.com>
2025-05-31 13:06:14 +0700

    uas selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+  static String nama = "Viona Amanda Putri";
+  static String gambar = "assets/gambar_dosen/viona.jpg";
 }
